aede3bd49981a73a801cf030cd58a0f30e0e0b15
kazak.ke <KazakKE@str.mos.ru>
2024-04-23 16:37:16 +0300

    add delivery

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from parsers import get_data_material_monitoring
 from config import data_path
 
 file_name = (
-    r"00_test_Отчет_март_2024.xlsx"  # Отчет март 2024.xlsx 00_test_Отчет_март_2024.xlsx
+    r"test_Отчет_март_2024.xlsx"  # Отчет март 2024.xlsx 00_test_Отчет_март_2024.xlsx
 )  # Отчет март 2024.xlsx 00_test_Отчет_март_2024.xlsx
 
 sheet_name = r"приложение А"
