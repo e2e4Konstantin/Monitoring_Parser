27fbaedf14125248918d11b963ebf0e0212c06e3
kazak.ke <KazakKE@str.mos.ru>
2024-04-25 18:23:28 +0300

    add pars transport cost

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
 from icecream import ic
-from parsers import get_data_material_monitoring
-from config import data_path
-from files_features import create_abspath_file
-
-import pandas
-
-# Отчет март 2024.xlsx 00_test_Отчет_март_2024.xlsx
-file_name = r"Отчет март 2024.xlsx"
-sheet_name = r"приложение А"
-
-csv_file_name = "monitoring_result.csv"
-parquet_file_name = "monitoring_result.parquet"
+from config import monitoring_data_place
+from parsers import monitoring_materials_parse, monitoring_transport_costs_parse
 
 
 if __name__ == "__main__":
-    result = get_data_material_monitoring(data_path, file_name, sheet_name)
-    ic(result[:5])
-
-    df = pandas.DataFrame(
-        result, columns=["code", "supplier_price", "delivery", "title"]
-    )
-    print(df)
-    csv_file = create_abspath_file(data_path, csv_file_name)
-    df.to_csv(csv_file, sep=",", index=False)
-
-    parquet_file = create_abspath_file(data_path, parquet_file_name)
-    df.to_parquet(parquet_file)
+    ic(monitoring_data_place)
+    # monitoring_materials_parse(monitoring_data_place)
+    # monitoring_transport_costs_parse(monitoring_data_place)
