889c8ef7e5e4d086d860ebc4d2f936d8af31b96a
kazak.ke <KazakKE@str.mos.ru>
2024-06-19 09:08:42 +0300

    55

--- files_features/file_tools.py
+++ files_features/file_tools.py
@@ -20,27 +20,33 @@ def handle_location(data_path: str, data_file: str):
     if not os.path.exists(src_file):
         output_message_exit("фал с данными не найден", f"{src_file!r}")
     if not os.path.isdir(output_path):
-        output_message_exit("папка для вывода фала с результатом не найдена", f"{output_path!r}")
+        output_message_exit(
+            "папка для вывода фала с результатом не найдена", f"{output_path!r}"
+        )
     return src_file, output_file
 
 
 def construct_abs_file_name(path: str, file_name: str) -> str:
-    """ Создает абсолютный маршрут к файлу. """
+    """Создает абсолютный маршрут к файлу."""
     if not os.path.isdir(path):
         output_message_exit("папка не найдена", f"{path!r}")
     return os.path.join(path, file_name)
 
-def create_abspath_file(path: str=None, file_name: str=None) -> str | None:
-    """ Создает абсолютный маршрут к файлу. Если путь не указан то берется место запуска."""
+
+def create_abspath_file(path: str = None, file_name: str = None) -> str | None:
+    """Создает абсолютный маршрут к файлу. Если путь не указан то берется место запуска."""
     if file_name:
-        path = pathlib.Path(path).resolve() if path else pathlib.Path(__file__).parent.resolve()
+        path = (
+            pathlib.Path(path).resolve()
+            if path
+            else pathlib.Path(__file__).parent.resolve()
+        )
         return pathlib.Path.joinpath(path, file_name).__str__()
     return None
 
 
-
 def file_exists(path: str) -> bool:
-    """ проверяет то что маршрут существует и это файл."""
+    """проверяет то что маршрут существует и это файл."""
     return pathlib.Path(path).is_file()
 
 
